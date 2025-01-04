de2c6759896bf530e3e47ba7efbeee0993693540
emmanuelcodechimp <emmanuel@codechimpit.com>
2025-01-04 12:23:09 +0000

    added framer motion for animation

--- src/components/WhyChooseUS.jsx
+++ src/components/WhyChooseUS.jsx
@@ -4,32 +4,101 @@ import { IoIosChatboxes } from "react-icons/io";
 import { GiWallet } from "react-icons/gi";
 import { FaCar } from "react-icons/fa6";
 
+//  animation with motion
+import { motion } from "motion/react";
 
 const WhyChooseUS = () => {
+
+  
+
+  const leftTextVariant = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.35,
+      },
+    },
+  };
+
+    const rightGridVariant = {
+      hidden: {
+        opacity: 0,
+      },
+      visible: {
+        opacity: 1,
+        transition: {
+          staggerChildren: 0.25,
+        },
+      },
+    };
+
+
+  const leftChildrenVariant = {
+    hidden: {
+      opacity: 0,
+      // y: 100,
+      scale: 0.7,
+      transition: { ease: [ "easeOut"] },
+    },
+    visible: {
+      opacity: 1,
+      // y: 0,
+      scale: 1,
+      transition: { ease: ["easeIn"] },
+    },
+  };
+
+   const rightGridChildrenVariant = {
+     hidden: {
+       opacity: 0,
+       transition: { ease: ["easeOut"] },
+     },
+     visible: {
+       opacity: 1,
+       transition: { ease: ["easeIn"] },
+     },
+   };
+
+
+
   return (
     <div>
       <div className="fifth">
         {/* left */}
-        <div className="left"> 
-          <div className="text">
-            <p>Why Choose Us?</p>
-            <h4>
+        <div className="left">
+          <motion.div
+            variants={leftTextVariant}
+            initial="hidden"
+            animate="visible"
+            className="text"
+          >
+            <motion.p variants={leftChildrenVariant}>Why Choose Us?</motion.p>
+            <motion.h4 variants={leftChildrenVariant}>
               To drive integrity by being honest & transparent in every
               interataction
-            </h4>
-            <span>
+            </motion.h4>
+            <motion.span variants={leftChildrenVariant}>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione
               debitis alias praesentium dolor expedita perspiciatis sequi facere
               voluptate esse iusto totam, natus ab laboriosam dolores pariatur
               omnis veniam! Ratione, soluta.20
-            </span>
-          </div>
+            </motion.span>
+          </motion.div>
         </div>
         {/* right */}
         <div className="right">
-          <div className="items-container" id="container">
-            {/* items */} 
-            <div className="item">
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={rightGridVariant}
+            className="items-container"
+            id="container"
+          >
+            {/* items */}
+            <motion.div variants={rightGridChildrenVariant} className="item">
               {/* icon */}
               <div className="icon">
                 <FaCar />
@@ -39,8 +108,8 @@ const WhyChooseUS = () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum,
                 blanditiis. Eveniet expedita dolorum ut neque.
               </span>
-            </div>
-            <div className="item">
+            </motion.div>
+            <motion.div variants={rightGridChildrenVariant} className="item">
               {/* icon */}
               <div className="icon">
                 <GrNotes />
@@ -50,8 +119,8 @@ const WhyChooseUS = () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum,
                 blanditiis. Eveniet expedita dolorum ut neque.
               </span>
-            </div>
-            <div className="item">
+            </motion.div>
+            <motion.div variants={rightGridChildrenVariant} className="item">
               {/* icon */}
               <div className="icon">
                 <IoIosChatboxes />
@@ -61,8 +130,8 @@ const WhyChooseUS = () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum,
                 blanditiis. Eveniet expedita dolorum ut neque.
               </span>
-            </div>
-            <div className="item">
+            </motion.div>
+            <motion.div variants={rightGridChildrenVariant} className="item">
               {/* icon */}
               <div className="icon">
                 <GiWallet />
@@ -72,8 +141,8 @@ const WhyChooseUS = () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum,
                 blanditiis. Eveniet expedita dolorum ut neque.
               </span>
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         </div>
       </div>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,26 +19,26 @@ const HomePage = () => {
     <div>
       <div className="conatiner">
         <div className="">
-          <Navbar />
+          {/* <Navbar /> */}
         </div>
         <div className="">
-          <HeroSection />
+          {/* <HeroSection /> */}
         </div>
 
         <div className="">
-          <Search />
+          {/* <Search /> */}
         </div>
         {/* second section is just a white space */}
         <div className="">
-          <SecondSection />
+          {/* <SecondSection /> */}
         </div>
 
         <div className="">
-          <AllCarSection/>
+          {/* <AllCarSection/> */}
         </div>
 
         <div className="">
-          <RatingSection/>
+          {/* <RatingSection/> */}
         </div>
 
         <div className="">
@@ -46,32 +46,32 @@ const HomePage = () => {
         </div>
 
         <div className="">
-          <BuyYourCar />
+          {/* <BuyYourCar /> */}
         </div>
 
         <div className="">
-          <CarListing />
+          {/* <CarListing /> */}
         </div>
 
         <div className="">
-          <BuyYourCarSection/>
+          {/* <BuyYourCarSection/> */}
         </div>
 
         <div className="">
-          <TestimonialSection/>
+          {/* <TestimonialSection/> */}
         </div>
         <div className="">
-          <Collaborators/>
+          {/* <Collaborators/> */}
         </div>
         <div className="">
-          <News/>
+          {/* <News/> */}
         </div>
         <div className="">
-          <GetStartedSection/>
+          {/* <GetStartedSection/> */}
         </div>
 
         <div className="">
-          <Footer/>
+          {/* <Footer/> */}
         </div>
       </div>
     </div>
